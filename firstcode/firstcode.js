79a6b08ac7b59e66fa5cdc7628463408148891b3
Chetan Anand <chetanguptasarai@gmail.com>
2020-10-30 19:41:52 +0530

    ok

--- firstcode/firstcode.js
+++ firstcode/firstcode.js
@@ -1,3 +1,52 @@
 // basic requirements to build a website 
 // html    css      js.
 
+// 
+// alert("me")
+// console.log("hello world")
+
+// document.write("this is document write")
+// 2
+// javascript console API
+
+// console.warn("this is warning")
+// console.error("this is error")
+// 3
+  var number1 = 34;
+  var number2 = 56;
+//  console.log(number1 + number2);
+// 4 data types in js
+// number
+  var num1 = 455;
+  var num2 = 215.50
+// string
+  var str1 = ("this is a string");
+  var str2 = ("this is also a string");
+// objects
+  var marks = {
+    ravi : 34,
+    shubham : 78,
+    harry : 79.25
+  }
+// booleans (true/false)
+  var a = true;
+  var b = false;
+  // console.log(a, b);
+
+// undefined/null
+  var und = undefined;
+  // console.log(und);
+
+  var n = null;
+  // console.log(n)
+
+
+//
+// there are two types of data types
+// 1 premitive dat types : undefined, null, number, string , boolean , symbol
+// 2 reference data types : arrays, objects
+
+  var arr = [1, 2, 3, 4, 5];
+
+  var arr1 = [5, 7, "bablu", 4, 5]
+
